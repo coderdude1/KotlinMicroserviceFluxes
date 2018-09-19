97af9538d5822c2210e5ff19ed570aa43fd22a62
Randy <drbugsmn@gmail.com>
2018-09-19 18:09:01 -0500

    update acc test to actually run and fail.  Update build.gradle with stuff that works/not deprecated

--- src/acceptance/groovy/com/dood/tdd/kotlinmicroservice/user/UserAcceptanceSpec.groovy
+++ src/acceptance/groovy/com/dood/tdd/kotlinmicroservice/user/UserAcceptanceSpec.groovy
@@ -1,8 +1,9 @@
 package com.dood.tdd.kotlinmicroservice.user
 
+import com.dood.tdd.kotlinmicroservice.users.model.User
+import groovyx.net.http.ContentType
 import groovyx.net.http.RESTClient
 import spock.lang.Specification
-
 
 class UserAcceptanceSpec extends Specification {
 
@@ -21,15 +22,24 @@ class UserAcceptanceSpec extends Specification {
 
     def 'add a new user'() {
         given:
-        def user = new User()
+        def user = new User('blarg', 'firstName', 'lastName')
 
         when:
-        def response = restClient.somethign
+//        def response = restClient.post(path : 'orders',
+//                requestContentType : ContentType.JSON,
+//                headers : ['Content-Type' : "application/json"],
+//                body    : [ 'id'    :   user.id,
+//                            'firstName' : user.firstName,
+//                            'lastNAME' : user.lastName])
+
+        def response
 
         then:
         response
         response.status == 200
         //I kknow there is a way to serailze this into a User
-        response.data[0].id = user.id
+        response.data[0].id == user.id
+        response.data[0].firstName == user.firstName
+        response.data[0].lastName == user.lastName
     }
 }
---
+++ src/main/kotlin/com/dood/tdd/kotlinmicroservice/users/model/User.kt
@@ -0,0 +1,3 @@
+package com.dood.tdd.kotlinmicroservice.users.model
+
+data class User(val id: String?, val firstName: String, val lastName: String)
